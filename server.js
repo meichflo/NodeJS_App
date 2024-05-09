8b8193e35afaee1e888887d9497f59c157f997e3
meichflo <meichflo@students.zhaw.ch>
2024-05-09 12:13:46 +0200

    Fix newline issue in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Florian to the DevOps Course \n :-P');
+    res.send('Hello Florian to the DevOps Course :-P');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
