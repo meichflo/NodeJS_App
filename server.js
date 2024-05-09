21346144e1df8948c88e0eed4202641b33dff0df
meichflo <meichflo@students.zhaw.ch>
2024-05-09 12:43:59 +0200

    Update server.js to include new message for DevOps Course

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Florian to the DevOps Course :-P. The new world is here! and the new world is DevOps!');
+    res.send('Hello Florian to the DevOps Course :-D | This is after the change of connecting the two jenkins items!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
