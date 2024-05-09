77bc56a753233dc96d55a2f46ea2055d76c9fcba
meichflo <meichflo@students.zhaw.ch>
2024-05-09 11:17:06 +0200

    Update greeting message in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello FS2024 DevOps Course!!!');
+    res.send('Hello Florian to the DevOps Course \n :-P');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
